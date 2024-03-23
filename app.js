49ba75d848831bbf77aca7d68efd09b436920563
Antoine-NVE <antoinenavette@gmail.com>
2024-03-23 13:44:02 +0100

    Episode 21 : Découverte du CRUD avec mongoDB

--- app.js
+++ app.js
@@ -7,12 +7,82 @@ async function main() {
     console.log('Connection OK');
     const db = client.db('myTask');
     const collection = db.collection('documents');
-    const insertStuff = await collection.insertMany([
-        { a: 1 },
-        { b: 2 },
-        { c: 3 },
-    ]);
-    console.log('Documents insérés :', insertStuff);
+
+    // CREATE
+    try {
+        const insertData = await collection.insertMany([
+            {
+                nom: 'Jean',
+                age: 30,
+                sexe: 'masculin',
+                hobby: 'jouer au football',
+            },
+            {
+                nom: 'Marie',
+                age: 25,
+                sexe: 'féminin',
+                hobby: 'peindre',
+            },
+            {
+                nom: 'Pierre',
+                age: 35,
+                sexe: 'masculin',
+                hobby: 'jouer de la guitare',
+            },
+        ]);
+        console.log('Documents insérés :', insertData);
+    } catch (error) {
+        throw error;
+    }
+
+    // READ
+    try {
+        const findData = await collection.findOne({ nom: 'Jean' });
+        console.log('Document trouvé :', findData);
+
+        const findMultipleData = collection.find({ sexe: 'masculin' });
+        console.log('Documents trouvés :', await findMultipleData.toArray());
+    } catch (error) {
+        throw error;
+    }
+
+    // UPDATE
+    try {
+        const updatePierre = collection.updateOne(
+            { nom: 'Pierre' },
+            {
+                $set: {
+                    nom: 'Pauline',
+                    sexe: 'féminin',
+                },
+            }
+        );
+        console.log('Document modifié :', await updatePierre);
+
+        const updateAge = collection.updateMany(
+            { age: 30 },
+            {
+                $set: {
+                    age: 31,
+                },
+            }
+        );
+        console.log('Documents modifiés :', await updateAge);
+    } catch (error) {
+        throw error;
+    }
+
+    // DELETE
+    try {
+        const deletePierre = await collection.deleteOne({ nom: 'Pauline' });
+        console.log('Document supprimé :', deletePierre);
+
+        const deleteEveryone = await collection.deleteMany();
+        console.log('Documents supprimés :', deleteEveryone);
+    } catch (error) {
+        throw error;
+    }
+
     return 'Done';
 }
 
