e084898f5e10013d2bd2d288eb8c8af7cdf4acf8
Antoine-NVE <antoinenavette@gmail.com>
2024-03-23 13:57:55 +0100

    Episode 22 : Présentation de mongoose (+ models)

--- app.js
+++ app.js
@@ -1,92 +1,27 @@
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
-const client = new MongoClient(process.env.MONGO_URL);
+const mongoose = require('mongoose');
+
+main().catch((err) => console.log(err));
 
 async function main() {
-    await client.connect();
-    console.log('Connection OK');
-    const db = client.db('myTask');
-    const collection = db.collection('documents');
+    await mongoose.connect(process.env.MONGO_URL);
 
-    // CREATE
-    try {
-        const insertData = await collection.insertMany([
-            {
-                nom: 'Jean',
-                age: 30,
-                sexe: 'masculin',
-                hobby: 'jouer au football',
-            },
-            {
-                nom: 'Marie',
-                age: 25,
-                sexe: 'féminin',
-                hobby: 'peindre',
-            },
-            {
-                nom: 'Pierre',
-                age: 35,
-                sexe: 'masculin',
-                hobby: 'jouer de la guitare',
-            },
-        ]);
-        console.log('Documents insérés :', insertData);
-    } catch (error) {
-        throw error;
-    }
+    const User = mongoose.model('User', {
+        name: String,
+        age: Number,
+    });
 
-    // READ
-    try {
-        const findData = await collection.findOne({ nom: 'Jean' });
-        console.log('Document trouvé :', findData);
+    const firstPerson = new User({
+        name: 'Antoine',
+        age: 20,
+    });
 
-        const findMultipleData = collection.find({ sexe: 'masculin' });
-        console.log('Documents trouvés :', await findMultipleData.toArray());
-    } catch (error) {
-        throw error;
-    }
+    const secondPerson = new User({
+        name: 'Justine',
+        age: 30,
+    });
 
-    // UPDATE
-    try {
-        const updatePierre = collection.updateOne(
-            { nom: 'Pierre' },
-            {
-                $set: {
-                    nom: 'Pauline',
-                    sexe: 'féminin',
-                },
-            }
-        );
-        console.log('Document modifié :', await updatePierre);
-
-        const updateAge = collection.updateMany(
-            { age: 30 },
-            {
-                $set: {
-                    age: 31,
-                },
-            }
-        );
-        console.log('Documents modifiés :', await updateAge);
-    } catch (error) {
-        throw error;
-    }
-
-    // DELETE
-    try {
-        const deletePierre = await collection.deleteOne({ nom: 'Pauline' });
-        console.log('Document supprimé :', deletePierre);
-
-        const deleteEveryone = await collection.deleteMany();
-        console.log('Documents supprimés :', deleteEveryone);
-    } catch (error) {
-        throw error;
-    }
-
-    return 'Done';
+    console.log(firstPerson, secondPerson);
+    await firstPerson.save();
+    await secondPerson.save();
 }
-
-main()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
